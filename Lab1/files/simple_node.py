29b2b15d775d251e1fa79e5632da5320d55c02b3
Ivan Penskiy <ipenskiy@umd.edu>
2024-02-15 14:40:38 -0500

    example files

---
+++ Lab1/files/simple_node.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+
+
+def main(args=None):
+    rclpy.init(args=args) #initialize
+    node = Node("py_test")
+    node.get_logger().info("Hello ROS2")
+    rclpy.shutdown() #node shutdown
+
+
+if __name__ == "__main__":
+    main()
