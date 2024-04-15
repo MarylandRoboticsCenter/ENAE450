fbadb372d3ae3d76aed88618e0aacfbe0fe1b26b
Ivan Penskiy <ipenskiy@umd.edu>
2024-04-14 21:27:41 -0400

    work on hw5

---
+++ homework/hw5/setup.py
@@ -0,0 +1,28 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'hw5'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mrc-ipen',
+    maintainer_email='mrc-ipen@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
